960990b3cbe0e9c459b48306c3533bbaba3ac4d0
Yash Garg <yashygkf@gmail.com>
2021-10-03 13:17:30 +0530

    movie model created

---
+++ lib/model/movie_model.dart
@@ -0,0 +1,15 @@
+class Movie {
+  String id;
+  String title;
+  String director;
+  String imgUrl;
+  bool status;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.director,
+    required this.status,
+    required this.imgUrl,
+  });
+}
