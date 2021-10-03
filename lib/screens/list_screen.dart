e6aa13cf762def06ad0c92080772380982a647cc
Yash Garg <yashygkf@gmail.com>
2021-10-03 13:18:15 +0530

    list screen completed

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/movie_list_provider.dart';
+
+import '../widgets/list__screen_header.dart';
+
+import 'add_screen.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({Key? key}) : super(key: key);
@@ -7,19 +15,190 @@ class ListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       body: Column(
         children: [
-          Container(
-            height: height * 0.3,
-            color: Colors.red,
+          ListScreenHeader(height: height, width: width),
+          ChangeNotifierProvider(
+            create: (context) => MovieListProvider(),
+            child: Container(
+              height: height * 0.64,
+              child: Consumer<MovieListProvider>(
+                builder: (context, movieList, child) => ListView.builder(
+                  itemBuilder: (ctx, i) => Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: height * 0.03,
+                      horizontal: width * 0.09,
+                    ),
+                    height: height * 0.4,
+                    width: width,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 0,
+                          bottom: height * 0.08,
+                          child: Material(
+                            elevation: 20,
+                            shadowColor: Colors.transparent,
+                            color: Colors.transparent,
+                            child: Container(
+                                alignment: Alignment.bottomRight,
+                                height: height * 0.22,
+                                width: width * 0.7,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromRGBO(152, 20, 255, 0.41),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    //color: Colors.yellow,
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  width: width * 0.39,
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: height * 0.02,
+                                    horizontal: width * 0.01,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          const Expanded(
+                                            child: Text(
+                                              "Movie: ",
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                              movieList.movies[i].title,
+                                              maxLines: 3,
+                                              softWrap: true,
+                                              overflow: TextOverflow.fade,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          const Expanded(
+                                            child: Text(
+                                              "Director: ",
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                              movieList.movies[i].director,
+                                              maxLines: 3,
+                                              softWrap: true,
+                                              overflow: TextOverflow.fade,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          IconButton(
+                                            onPressed: () {
+                                              movieList.movieStatus(
+                                                  movieList.movies[i]);
+                                              ScaffoldMessenger.of(context)
+                                                  .showSnackBar(
+                                                SnackBar(
+                                                  content: movieList
+                                                          .movies[i].status
+                                                      ? const Text(
+                                                          "Movie Marked Watched")
+                                                      : const Text(
+                                                          "Movied Marked Unwatched",
+                                                        ),
+                                                ),
+                                              );
+                                            },
+                                            icon: movieList.movies[i].status
+                                                ? const FaIcon(
+                                                    FontAwesomeIcons.solidEye,
+                                                  )
+                                                : const FaIcon(
+                                                    FontAwesomeIcons.eye,
+                                                  ),
+                                          ),
+                                          IconButton(
+                                            onPressed: () {
+                                              movieList.delMovie(
+                                                  movieList.movies[i]);
+                                              ScaffoldMessenger.of(context)
+                                                  .showSnackBar(
+                                                const SnackBar(
+                                                  content: Text(
+                                                    "Movie Deleted from the list",
+                                                  ),
+                                                ),
+                                              );
+                                            },
+                                            icon: const FaIcon(
+                                              FontAwesomeIcons.trashAlt,
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                )),
+                          ),
+                        ),
+                        Positioned(
+                          child: Material(
+                            elevation: 20,
+                            color: Colors.transparent,
+                            child: Container(
+                              height: height * 0.3,
+                              width: width * 0.45,
+                              decoration: BoxDecoration(
+                                color: Colors.yellow,
+                                borderRadius: BorderRadius.circular(30),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    movieList.movies[i].imgUrl,
+                                  ),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  itemCount: movieList.movies.length,
+                ),
+              ),
+            ),
           ),
           Container(
-            height: height * 0.6,
-          ),
-          Container(
-            height: height * 0.1,
-            color: Colors.blue,
+            height: height * 0.06,
+            color: Colors.transparent,
+            child: Center(
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(AddScreen.routeName);
+                },
+                icon: const FaIcon(
+                  FontAwesomeIcons.plusCircle,
+                ),
+                iconSize: height * 0.04,
+              ),
+            ),
           ),
         ],
       ),
---
+++ lib/widgets/list__screen_header.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class ListScreenHeader extends StatelessWidget {
+  const ListScreenHeader({
+    Key? key,
+    required this.height,
+    required this.width,
+  }) : super(key: key);
+
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height * 0.3,
+      width: width,
+      child: Stack(
+        children: [
+          const Positioned(
+            top: 0,
+            left: 0,
+            child: Image(
+              image: AssetImage(
+                "assets/images/top_left.png",
+              ),
+              fit: BoxFit.fill,
+            ),
+          ),
+          const Positioned(
+            top: 0,
+            right: 0,
+            child: Image(
+              image: AssetImage(
+                "assets/images/top_right.png",
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: height * 0.03,
+            left: width * 0.1,
+            child: Material(
+              elevation: 10,
+              color: Colors.transparent,
+              shadowColor: Colors.transparent,
+              child: Container(
+                width: width * 0.5,
+                height: height * 0.1,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: const Color.fromRGBO(152, 20, 255, 0.41),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade100,
+                      offset: Offset.fromDirection(1, 4),
+                      spreadRadius: 5,
+                      blurRadius: 8,
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text("Hey Yash! \n Here is Your Movie List "),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
